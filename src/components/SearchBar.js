debc0f914c254fc479d3642f66686be71b872aca
github username <github email address>
2019-04-09 21:01:15 -0400

    Done.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,32 +1,54 @@
 import React from 'react';
 
-const SearchBar = () => {
-  return (
-    <div>
+class SearchBar extends React.Component {
+  state = {
+    sortBy: ''
+  }
 
-      <strong>Sort by:</strong>
-      <label>
-        <input type="radio" value="Alphabetically" checked={null} onChange={null}/>
-        Alphabetically
-      </label>
-      <label>
-        <input type="radio" value="Price" checked={null} onChange={null}/>
-        Price
-      </label>
-      <br/>
+  handleRadioButton = event => {
+    this.setState({
+      sortBy: event.target.value
+    })
 
-      <label>
-        <strong>Filter:</strong>
-        <select onChange={null}>
-          <option value="Tech">Tech</option>
-          <option value="Sportswear">Sportswear</option>
-          <option value="Finance">Finance</option>
-        </select>
-      </label>
+    this.props.handleSort(event.target.value)
+  }
+
+  render() {
+    return (
+      <div>
+        <strong>Sort by:</strong>
+        <label>
+          <input
+            type="radio"
+            value="Alphabetically"
+            checked={this.state.sortBy === 'Alphabetically'}
+            onChange={this.handleRadioButton} />
+          Alphabetically
+        </label>
+        <label>
+          <input
+            type="radio"
+            value="Price"
+            checked={this.state.sortBy === 'Price'}
+            onChange={this.handleRadioButton} />
+          Price
+        </label>
+        <br/>
+
+        <label>
+          <strong>Filter:</strong>
+          <select onChange={(event) => this.props.handleFilter(event.target.value)}>
+            <option value="All">All</option>
+            <option value="Tech">Tech</option>
+            <option value="Sportswear">Sportswear</option>
+            <option value="Finance">Finance</option>
+          </select>
+        </label>
 
 
-    </div>
-  );
+      </div>
+    )
+  }
 }
 
 
--- src/containers/PortfolioContainer.js
+++ src/containers/PortfolioContainer.js
@@ -4,11 +4,14 @@ import Stock from '../components/Stock'
 class PortfolioContainer extends Component {
 
   render() {
+    const soldStock = this.props.sold.map(sold =>{
+      return <Stock handleClick={this.props.sellStock} stock={sold}/>
+    })
     return (
       <div>
         <h2>My Portfolio</h2>
           {
-            //render your portfolio stocks here
+            soldStock
           }
       </div>
     );
